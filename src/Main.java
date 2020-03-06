0e304b5d9ab9b3f843ebb9fcce2926b9f2153a76
Kevulis <56118071+Kevulis@users.noreply.github.com>
2020-03-06 13:18:32 +0000

    Testing
    testing

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import cards.Card;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Card myMaster=Factory.getCard(123456, "Almicar Aponte", 456, "Mastercard");
+		System.out.println(myMaster);
+	}
+
+}
